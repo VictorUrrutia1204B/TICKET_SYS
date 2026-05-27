a264d735d298757bf4e23c111bc953aeff1e0c57
VictorUrrutia1204B <urrutiavictor609@gmail.com>
2026-05-26 21:53:06 -0600

    Update BD.sql

--- Backend/BD.sql
+++ Backend/BD.sql
@@ -1,1 +1,8 @@
-CREATE DATABASE TICKETS
+CREATE DATABASE TICKETS(
+  Id_ticket NUMBER PRIMARY_KEY,
+titulo VARCHAR (100)NOT NULL,
+descripción VARCHAR (500),
+fecha_creacion DATE,
+estado VARCHAR(20)
+);
+
